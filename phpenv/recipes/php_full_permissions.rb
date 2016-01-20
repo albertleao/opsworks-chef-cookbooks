41674d73fd097346810dc7a7175052aa58bf2d71
Albert Leao <albertleao@live.com>
2016-01-20 15:33:26 -0500

    added vendor to chmod

--- phpenv/recipes/php_full_permissions.rb
+++ phpenv/recipes/php_full_permissions.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |application, deploy|
   app_root = "#{deploy[:deploy_to]}/current"
-  execute "chmod -R g+rw #{app_root}" do
+  execute "chmod -R g+rw #{app_root}/vendor" do
   end
 end
