e4daa6dba4c41e0de144c3a756931dece70789b4
Albert Leao <albertleao@live.com>
2016-01-20 16:08:33 -0500

    added separate perms

--- phpenv/recipes/php_full_permissions.rb
+++ phpenv/recipes/php_full_permissions.rb
@@ -1,5 +1,7 @@
 node[:deploy].each do |application, deploy|
   app_root = "#{deploy[:deploy_to]}/current"
-  execute "chmod -R g+rw #{app_root}/vendor" do
+  execute "chmod -R g+rw #{app_root}" do
+  end
+  execute "chmod -R 0777 #{app_root}/vendor" do
   end
 end
