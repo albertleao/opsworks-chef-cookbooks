e2d11da7247fac281f66c6738e893d8419253141
Albert Leao <albertleao@live.com>
2016-12-20 18:15:52 -0500

    made different commands

--- phpenv/recipes/php_upgrade_latest.rb
+++ phpenv/recipes/php_upgrade_latest.rb
@@ -4,15 +4,15 @@ when "debian","ubuntu"
 
   Chef::Log.debug("Upgrading to php 7.0")
 
-  execute "php_upgrade_latest" do
+  execute "remove_php_5" do
     user "root"
     command "apt-get purge 'php5*'"
-    command "add-apt-repository ppa:ondrej/php"
-    command "apt-get update"
-    command "apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mysql mcrypt php7.0-mcrypt imagemagick php-imagick php7.0-curl php7.0-gd php7.0-intl php7.0-imap php7.0-xmlrpc  php7.0-pspell php7.0-recode php7.0-tidy php-apcu php7.0-json php7.0-mbstring php7.0-xml php7.0-bcmath php-redis php-yaml  php-gettext php-pear"
-    command "apt-get autoremove"
-    command "service apache2 restart"
+    action :run, 'execute[add_ppa]', :immediately
+  end
 
-    action :run
+  execute "remove_php_5" do
+    user "root"
+    command "add-apt-repository ppa:ondrej/php"
+    action :nothing
   end
 end
