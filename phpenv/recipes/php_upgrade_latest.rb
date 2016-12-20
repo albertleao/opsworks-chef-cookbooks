c0e965e7e38d38f2dce2a9a2a6fe51253126d624
Albert Leao <albertleao@live.com>
2016-12-20 18:31:50 -0500

    added force yes

--- phpenv/recipes/php_upgrade_latest.rb
+++ phpenv/recipes/php_upgrade_latest.rb
@@ -25,13 +25,13 @@ when "debian","ubuntu"
 
   execute "install_latest_php" do
     user "root"
-    command "apt-get -y install php7.0 libapache2-mod-php7.0 php7.0-mysql mcrypt php7.0-mcrypt imagemagick php-imagick php7.0-curl php7.0-gd php7.0-intl php7.0-imap php7.0-xmlrpc  php7.0-pspell php7.0-recode php7.0-tidy php-apcu php7.0-json php7.0-mbstring php7.0-xml php7.0-bcmath php-redis php-yaml php-gettext php-pear"
+    command "apt-get install php7.0 libapache2-mod-php7.0 php7.0-mysql mcrypt php7.0-mcrypt imagemagick php-imagick php7.0-curl php7.0-gd php7.0-intl php7.0-imap php7.0-xmlrpc  php7.0-pspell php7.0-recode php7.0-tidy php-apcu php7.0-json php7.0-mbstring php7.0-xml php7.0-bcmath php-redis php-yaml php-gettext php-pear -y --force-yes"
     notifies :run, 'execute[cleanup_install]', :immediately
   end
 
   execute "cleanup_install" do
     user "root"
-    command "apt-get autoremove -y"
+    command "apt-get autoremove -y --force-yes"
     notifies :run, 'execute[restart_server]', :immediately
   end
 
