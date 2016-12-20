2b843fb301588404e418e2b866bb4843994f7a4c
Albert Leao <albertleao@live.com>
2016-12-20 17:29:42 -0500

    upgrade to php7 recipe

--- phinx/metadata.rb
+++ phinx/metadata.rb
@@ -1,6 +1,6 @@
 name              "phinx"
 maintainer        "albertleao"
-maintainer_email  "kinojman@gmail.com"
+maintainer_email  "albertleao@live.com"
 license           "MIT"
 description       "Performs PHINX migrations"
 version           "1.0.0"
